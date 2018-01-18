a38f491cdd2bea06975d677da2a0e84782d8c076
Andrey Dats <notmymail@example.com>
2018-01-18 17:20:51 +0200

    Starting configuration for application.

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=db_for_blog',
+    'username' => 'yiiUser',
+    'password' => 'IKP30ApnzsidRhUoIUS6',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
